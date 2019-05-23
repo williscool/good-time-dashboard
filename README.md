0bc730d6ff02b94bb62a8fbee097ca726aa70b23
William Harris <william@lyric.com>
2019-05-23 02:07:58 -0700

    actually get something from the eventbrite api

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # good-time-dashboard
 Get events in the area in json. Import them into google sheets. Profit (from search)
+
+
+# insprations
+
+https://www.mapdevelopers.com/draw-circle-tool.php?circles=%5B%5B80467%2C37.7749295%2C-122.4194155%2C%22%23AAAAAA%22%2C%22%23000000%22%2C0.4%5D%5D
+
+
+https://stackoverflow.com/questions/16316330/how-to-write-file-if-parent-folder-doesnt-exist/42029542#42029542
--- src/index.js
+++ src/index.js
@@ -1,10 +1,61 @@
-import commander from "commander";
+import dotenv from "dotenv"
+import commander from "commander"
+import eventbrite from "eventbrite"
+import to from "await-to-js"
+import path from "path"
+import fs from "fs-extra"
+import util from "util"
+
+dotenv.config()
+
+const DEFAULT_REPORT_NAME = "report_from_today"
+const { EVENTBRITE_OAUTH_TOKEN } = process.env
 
 commander
-  .option('-c, --cheese [type]', 'Add cheese with optional type');
+  .description("Good Time Dashboard Data Generator")
+  .option("-o, --output-filename", `name of the output json. defaults to ${DEFAULT_REPORT_NAME}`)
 
-commander.parse(process.argv);
+commander.parse(process.argv)
 
-if (commander.cheese === undefined) console.log('no cheese');
-else if (commander.cheese === true) console.log('add cheese');
-else console.log(`add cheese type ${commander.cheese}`);
+async function getEventsFromEventBrite() {
+  // Create configured Eventbrite SDK
+  const sdk = eventbrite({ token: EVENTBRITE_OAUTH_TOKEN })
+
+  // See: https://www.eventbrite.com/developer/v3/endpoints/users/#ebapi-get-users-id
+  const [err, data] = await to(sdk.request("/users/me"))
+
+  if (err) {
+    console.error(err)
+    process.exit(0)
+  }
+
+  return JSON.stringify(data)
+}
+
+async function writeJSON() {
+  let reportName = DEFAULT_REPORT_NAME
+
+  if (commander.outputFilename) {
+    reportName = commander.outputFilename
+  }
+
+  const outputPathObject = {
+    dir: "tmp",
+    name: reportName,
+    ext: ".json"
+  }
+
+  const OUTPUT_FILE_PATH = path.format(outputPathObject)
+  const outputFile = util.promisify(fs.outputFile)
+
+  const [fileReadErr,] = await to(outputFile(OUTPUT_FILE_PATH, await getEventsFromEventBrite()))
+
+  if (fileReadErr) {
+    console.error(fileReadErr)
+    process.exit(0)
+  }
+
+  console.log(`${OUTPUT_FILE_PATH} was saved!`)
+}
+
+writeJSON();
